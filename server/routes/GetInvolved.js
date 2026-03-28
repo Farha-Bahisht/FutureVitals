23d7dbce4cecb33bdf4ff0c48a7ae1f90ed59209
PranavKheny <pranavkheny@gmail.com>
2026-03-28 01:33:32 -0400

    fix: routes issue

--- server/routes/GetInvolved.js
+++ server/routes/GetInvolved.js
@@ -1,4 +1,4 @@
-// server/routes/getinvolved.js
+// server/routes/GetInvolved.js
 const express = require("express");
 const multer = require("multer");
 const cloudinary = require("cloudinary").v2;
